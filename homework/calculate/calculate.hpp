5f3810524935e06afabf7ea14efb63feedce4ba7
Milena Sobotka <s171809@student.pg.edu.pl>
2024-01-31 17:00:53 +0100

    Adding divide command and exception handling

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -14,6 +14,16 @@ std::string calculate(const std::string& command, int first, int second) {
         int result = first * second;
         return std::to_string(result);
     } 
-
-    return "";
+    else if(command == "divide"){
+        if(second != 0){
+            int result = first / second;
+            return std::to_string(result);
+        }
+        else{
+            return "Division by 0";
+        }
+    } 
+    else{
+        return "Invalid data";
+    }
 }
