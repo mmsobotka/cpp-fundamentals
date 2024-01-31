5801a6028c16e6217ad8dabc493b63975022dc84
Milena Sobotka <s171809@student.pg.edu.pl>
2024-01-31 16:50:19 +0100

    Adding add command

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,10 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
-    // TODO: Implement your solution here and return proper value
+    if(command == "add"){
+        int result = first + second;
+        return std::to_string(result);
+    } 
+
     return "";
 }
