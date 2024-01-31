8d396808e7e649a5d124ec601b20d55fc567c699
mmsobotka <milenasobotka@gmail.com>
2024-01-31 17:41:14 +0100

    Add empty line

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -2,6 +2,7 @@
 #include <string>
 
 std::string calculate(const std::string& command, int first, int second) {
+    
     if(command == "add"){
         int result = first + second;
         return std::to_string(result);
